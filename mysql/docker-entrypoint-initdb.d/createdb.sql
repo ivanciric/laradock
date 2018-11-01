b8a6d2f1328f23586ac4f814a61e0fa85508a6be
Ivan Ciric <info@yoshi.tech>
2018-11-01 12:02:35 +0100

    Setting up mysql

--- mysql/docker-entrypoint-initdb.d/createdb.sql
+++
@@ -1,8 +0,0 @@
-CREATE USER 'admin'@'%' IDENTIFIED WITH mysql_native_password BY 'admin';
-GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;
-CREATE USER 'admin'@'%' IDENTIFIED WITH mysql_native_password BY 'admin';
-GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;
-#
-CREATE DATABASE IF NOT EXISTS `pim` COLLATE 'utf8_general_ci' ;
-GRANT ALL ON `pim`.* TO 'admin'@'%' ;
-FLUSH PRIVILEGES ;
